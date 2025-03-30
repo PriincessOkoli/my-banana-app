f1960071dbc0db7db75b3b75c7f9ca822ebaf30f
okoli Chimarume <juskie115@gmail.com>
2025-03-30 15:28:38 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const products = [
+    { image: "/images/image.png", name: "Cavendish Banana", price: "$1.00" },
+    { image: "images/image7.png", name: "Plantain Banana", price: "$1.20" },
+    { image: "images/image4.png", name: "Red Banana", price: "$1.50" },
+    { image: "images/image3.png", name: "Musa Velutina", price: "$1.80" },
+    { image: "images/image6.png", name: "Musa Nanensis", price: "$2.00" },
+    { image: "images/image5.png", name: "Manzano Banana", price: "$2.50" },
+    { image: "images/image10.png", name: "Blue Java Banana", price: "$3.00" },
+    { image: "images/image8.png", name: "Mutahato Banana", price: "$3.50" },
+    { image: "/images/image2.png", name: "Hardly Banana", price: "$4.00" },
+    { image: "images/image9.png", name: "Purple Banana", price: "$4.50" }
+  ];
+
+  let currentIndex = 0;
+  const slider = document.getElementById("slider");
+
+  function displayProduct(index) {
+    const { image, name, price } = products[index];
+    slider.innerHTML = `<img src="${image}" alt="${name}"><div class='product-info'><b>${name}</b><br>${price}</div><button>Add to Cart</button>`;
+  }
+  
+  function prevSlide() {
+    currentIndex = currentIndex > 0 ? currentIndex - 1 : products.length - 1;
+    displayProduct(currentIndex);
+  }
+  
+  function nextSlide() {
+    currentIndex = currentIndex < products.length - 1 ? currentIndex + 1 : 0;
+    displayProduct(currentIndex);
+  }
+  
+  displayProduct(currentIndex);
